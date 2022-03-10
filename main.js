d4004dc8fa6cab5fb1deaf3b3bb64e3396f3712f
Daniel Anthes <daniel.anthes97@gmail.com>
2022-03-10 14:52:47 +0100

    start game on startup

--- main.js
+++ main.js
@@ -5,8 +5,9 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 256,
+    height: 512,
+    useContentSize: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
